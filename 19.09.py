7398c9d3e99acefb6ce4322b56204390d348c2f7
rtrwslv <yarsl.filatov@gmail.com>
2022-10-24 22:40:45 +0300

    Начал добавлять домашки по датам

---
+++ 19.09.py
@@ -0,0 +1,26 @@
+from tabnanny import check
+
+
+def date_check(day, month, year):
+    """
+        Function validate date and return True or False as a result
+    """
+    leap = False
+    day_max = 30
+    if year > 0:
+        if year % 100 == 0:
+            leap = False
+        if year % 4 == 0 or year & 400 == 0:
+            leap = True
+        if 12 >= month > 1:
+            if month % 2 == 0:
+                day_max = 31
+                if month == 2 and leap:
+                    day_max = 29
+                elif month == 2 and not leap:
+                    day_max = 28
+            if day <= day_max:
+                return True
+    return False
+
+print(date_check(10, 12, 2022))
