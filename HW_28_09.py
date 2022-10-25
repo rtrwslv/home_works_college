9f11f45bdd76c2d668fc80fadc256dbe2506ec07
rtrwslv <83144402+rtrwslv@users.noreply.github.com>
2022-10-25 11:38:48 +0300

    Update HW_28_09.py

--- HW_28_09.py
+++ HW_28_09.py
@@ -1,1 +1,15 @@
+def diary(records, start_date):
+    """Function makes file with pirate diary, using given arguments"""
+    file = open("diary", "w")
+    for i in range(len(records)):
+        file.write(
+            str(
+                (
+                    datetime.strptime(start_date, "%d-%m-%Y") + timedelta(days=i)
+                ).strftime("%d-%m-%Y")
+            )
+            + " "
+            + str(records[i])
+            + "\n"
+        )
 
