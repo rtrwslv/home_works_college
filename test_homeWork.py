16291cff48e87bd419c051aa071ac985aa7f9cab
rtrwslv <83144402+rtrwslv@users.noreply.github.com>
2022-10-25 11:39:54 +0300

    Update test_homeWork.py

--- test_homeWork.py
+++ test_homeWork.py
@@ -6,7 +6,7 @@ tests = [("ABc dbE FRl Ama", "50%"), ("NDp Lka nuR vtE", "25%")]
 
 @pytest.mark.parametrize('input_string, result', tests)
 def test_digit_count(input_string, result):
-    assert HW.digit_count(input_string) == result
+    assert Hw.digit_count(input_string) == result
 
 
 test_date = [(29, 12, 2022, True)]
@@ -14,4 +14,4 @@ test_date = [(29, 12, 2022, True)]
 
 @pytest.mark.parametrize('day, month, year, result', test_date)
 def test_date_check(day, month, year, result):
-    assert HW.date_check(day, month, year) == result
+    assert Hw.date_check(day, month, year) == result
